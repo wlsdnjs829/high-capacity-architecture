244acc93e74c82f71499d932ada45cb94ca28252
ljw0829 <olo_jinwon@naver.com>
2022-10-01 00:07:47 +0900

    - feat : Redis를 활용한 이벤트 처리

--- src/main/kotlin/com/example/high/capacity/architecture/LoadController.kt
+++ src/main/kotlin/com/example/high/capacity/architecture/LoadController.kt
@@ -1,28 +1,41 @@
 package com.example.high.capacity.architecture
 
-import java.lang.Exception
 import java.util.Stack
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class LoadController {
+class LoadController(
+    private val redisTemplate: RedisTemplate<String, String>,
+) {
 
-    private final val queue: Stack<Int> = Stack()
+    companion object {
+        private const val KEY = "CAPACITY_TEST"
+    }
 
     init {
-        for (data in 1..10000) {
-            queue.push(data)
-        }
+        val opsForValue = redisTemplate.opsForValue()
+        opsForValue.set(KEY, "0")
     }
 
     @GetMapping
     fun load(): String {
-        if (queue.size == 0) {
+        val opsForValue = redisTemplate.opsForValue()
+
+        val data = opsForValue.get(KEY)?.toLong() ?: throw Exception("잘못된 타입")
+
+        if (data > 10000L) {
             throw Exception("없엉")
         }
 
-        return queue.pop().toString()
+        val increment = opsForValue.increment(KEY) ?: throw Exception("잘못된 타입")
+
+        if (increment > 10000L) {
+            throw Exception("없엉")
+        }
+
+        return increment.toString()
     }
 
 }
---
+++ src/main/kotlin/com/example/high/capacity/architecture/RedisConfig.kt
@@ -0,0 +1,31 @@
+package com.example.high.capacity.architecture
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+
+@Configuration
+class RedisConfig {
+
+    @Value("\${spring.redis.host}")
+    private lateinit var host: String
+
+    @Value("\${spring.redis.port}")
+    private val port: Int = 0
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory(host, port)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<*, *> {
+        val redisTemplate: RedisTemplate<*, *> = RedisTemplate<Any, Any>()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        return redisTemplate
+    }
+
+}
