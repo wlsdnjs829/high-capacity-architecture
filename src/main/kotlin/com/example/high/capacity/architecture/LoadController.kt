9195c87045b9bf203e500593dafa3a3e3fc2e1db
ljw0829 <olo_jinwon@naver.com>
2022-09-30 23:15:25 +0900

    - feat : Thread Safe한 Stack을 활용해서 이벤트 기능 구현을 해보자.

--- src/main/kotlin/com/example/high/capacity/architecture/LoadController.kt
+++ src/main/kotlin/com/example/high/capacity/architecture/LoadController.kt
@@ -1,15 +1,28 @@
 package com.example.high.capacity.architecture
 
+import java.lang.Exception
+import java.util.Stack
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class LoadController {
 
+    private final val queue: Stack<Int> = Stack()
+
+    init {
+        for (data in 1..10000) {
+            queue.push(data)
+        }
+    }
+
     @GetMapping
     fun load(): String {
-        println("logging")
-        return "JINWON"
+        if (queue.size == 0) {
+            throw Exception("없엉")
+        }
+
+        return queue.pop().toString()
     }
 
 }
